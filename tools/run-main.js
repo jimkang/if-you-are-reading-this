b7708fbe7b5f8444bbca5568631f2b3b984112f3
Jim Kang <jimkang@gmail.com>
2015-11-30 08:50:07 -0500

    Tool supports alternate root and length control.

--- tools/run-main.js
+++ tools/run-main.js
@@ -1,13 +1,24 @@
 var tracery = require('tracery-grammar');
 var jsonfile = require('jsonfile');
 
+var root = 'origin';
+var n = 100;
+
+if (process.argv.length > 3) {
+  n = process.argv[3];
+}
+
+if (process.argv.length > 2) {
+  root = process.argv[2];
+}
+
 var grammarSpec = jsonfile.readFileSync(__dirname + '/../data/cbdq.json');
 var processedGrammar = tracery.createGrammar(grammarSpec);
 
 var validTweetCount = 0;
 
-for (var i = 0; i < 100; ++i) {
-  var resolved = processedGrammar.flatten('#origin#');
+for (var i = 0; i < n; ++i) {
+  var resolved = processedGrammar.flatten('#' + root + '#');
   console.log(resolved);
   console.log('\n');
   console.log('Length:', resolved.length);
