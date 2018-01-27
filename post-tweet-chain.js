42526f402cbb3c728ea82be5ed7934d5fcfe8f64
Jim Kang <jimkang@gmail.com>
2018-01-26 20:38:24 -0500

    Post to archive.

--- post-tweet-chain.js
+++ post-tweet-chain.js
@@ -10,6 +10,24 @@ var jsonfile = require('jsonfile');
 var grammarSpec = jsonfile.readFileSync(__dirname + '/data/cbdq.json');
 var processedGrammar = tracery.createGrammar(grammarSpec);
 
+var StaticWebArchiveOnGit = require('static-web-archive-on-git');
+var queue = require('d3-queue').queue;
+var randomId = require('idmaker').randomId;
+
+var staticWebStream = StaticWebArchiveOnGit({
+  config: config.github,
+  title: config.archiveName,
+  footerScript: `<script type="text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49491163-1', 'jimkang.com');
+  ga('send', 'pageview');
+</script>`,
+  maxEntriesPerPage: 50
+});
+
 var decorators = [
   '🔔',
   '🔊',
@@ -35,11 +53,11 @@ var twit = new Twit(config.twitter);
 var statements = getStatements();
 var resolvedText = statements.join(' ');
 if (resolvedText.length < 100 && probable.roll(2) === 0) {
-  postTweets([resolvedText, getRepeatText(), resolvedText], wrapUp);
+  postToTargets([resolvedText, getRepeatText(), resolvedText], wrapUp);
 } else if (resolvedText.length <= 140) {
-  postTweets([resolvedText], wrapUp);
+  postToTargets([resolvedText], wrapUp);
 } else {
-  postTweets(statements, wrapUp);
+  postToTargets(statements, wrapUp);
 }
 
 function getStatements() {
@@ -74,13 +92,7 @@ function randomlyCap(s) {
 }
 
 function postTweets(parts, done) {
-  if (dryRun) {
-    var combinedStatement = parts.join(' ');
-    console.log('Would have tweeted:', combinedStatement);
-    callNextTick(done);
-  } else {
-    postNextTweet(null, done);
-  }
+  postNextTweet(null, done);
 
   function postNextTweet(lastTweet, done) {
     var text = '';
@@ -126,6 +138,29 @@ function postTweets(parts, done) {
       }
     }
   }
+}
+
+function postToTargets(parts, done) {
+  var combinedStatement = parts.join(' ');
+  if (dryRun) {
+    console.log('Would have tweeted:', combinedStatement);
+    callNextTick(done);
+  } else {
+    var q = queue();
+    q.defer(postTweets, parts);
+    q.defer(postToArchive, combinedStatement);
+    q.await(done);
+  }
+}
+
+function postToArchive(text, done) {
+  var id = 'alert-' + randomId(8);
+  staticWebStream.write({
+    id,
+    date: new Date().toISOString(),
+    caption: text
+  });
+  staticWebStream.end(done);
 }
 
 function wrapUp(error, data) {
