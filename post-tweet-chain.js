ecafe166e5920e6a42c69419f07eb15b52863e5f
Jim Kang <jimkang@gmail.com>
2018-01-31 09:45:19 -0500

    Add footer to archive posts.

--- post-tweet-chain.js
+++ post-tweet-chain.js
@@ -17,7 +17,9 @@ var randomId = require('idmaker').randomId;
 var staticWebStream = StaticWebArchiveOnGit({
   config: config.github,
   title: config.archiveName,
-  footerScript: `<script type="text/javascript">
+  footerHTML: `<footer>Created by <a href="https://twitter.com/NotInventedHere">@NotInventedHere</a>, <a href="https://twitter.com/metroidbaby">@metroidbaby</a> and me.
+  </footer>
+  <script type="text/javascript">
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
